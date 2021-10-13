58246621ca617c00a57bcb6b3a916629b55e4db0
yuanmeda <y243139883@163.com>
2021-10-13 08:13:21 +0800

    学习笔记

---
+++ admin/src/pages/SourceCode/Axios/axios.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: 钟媛
+ * @Date: 2021-09-16 22:41:03
+ * @LastEditTime: 2021-09-16 23:12:56
+ * @Description: 尝试模拟实现一个axios
+ */
+
+// 1. axios(config) 传入一个配置对象，返回一个promise
+
+function axios(config) {
+  return new Promise((resolve, reject) => {
+
+  })
+}
